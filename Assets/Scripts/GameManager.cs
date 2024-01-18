ff73eaa0dd5ae7cd50d425d7ca7d883ec9ca32cf
BananaDude508 <malsebhal@gmail.com>
2024-01-18 13:42:24 +1030

    Added enemy, gameover and various changes
    Added the ability to damage enemy, enemy damage you, lose, fight boss (unsure if functioning) plus various changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    #region Various Decs.
+
     [SerializeField]
     GameObject[] shopList;
     int[] speedCosts, strengthCosts, defenceCosts, healthCosts;
     TextMeshProUGUI[] speedUpgradeTexts, strengthUpgradeTexts, defenceUpgradeTexts, healthUpgradeTexts;
     int currentIndex = 0;
 
+    int currentEnemy = 0;
+
+    private float tPlayer, tEnemy;
+
 
     [SerializeField]
     PlayerStats stats = new PlayerStats(1, 10, 0, 100, 1000);
+    [SerializeField]
+    EnemyStats enemyStats = new EnemyStats(1, 10, 0, 100);
 
     public TextMeshProUGUI moneyText;
-    public TextMeshProUGUI speedText, strengthText, defenceText, healthText, healthSliderText;
-    public Slider healthSlider;
+    public TextMeshProUGUI speedText, strengthText, defenceText, healthText, healthSliderText, enemySliderText;
+    public Slider healthSlider, enemySlider;
+
+    void OnEnable()
+    {
+        tPlayer = 0f;
+        tEnemy = 0f;
+    }
+
+    #endregion
 
     void Start()
     {
@@ -39,8 +56,8 @@ public class GameManager : MonoBehaviour
         for (int i = 1; i < shopList.Length; i++)
         {
             speedCosts[i] = speedCosts[i-1] * 4 * i;
-            strengthCosts[i] = speedCosts[i-1] * 4 * 1;
-            defenceCosts[i] = speedCosts[i-1] * 4* i;
+            strengthCosts[i] = speedCosts[i-1] * 4 * i;
+            defenceCosts[i] = speedCosts[i-1] * 4 * i;
             healthCosts[i] = speedCosts[i-1] * 4 * i;
         }
         for (int i = 0; i < shopList.Length; i++)
@@ -50,12 +67,31 @@ public class GameManager : MonoBehaviour
             defenceUpgradeTexts[i] = shopList[i].transform.GetChild(2).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             healthUpgradeTexts[i] = shopList[i].transform.GetChild(3).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
         }
+        enemyStats.NewStats(stats);
+        currentEnemy++;
     }
 
     void Update()
     {
         UpdateUIDisplay();
+
+        HurtPlayer(enemyStats.Strength(), enemyStats.Speed());
+        HurtEnemy(stats.Strength(), stats.Speed());
+
+        if (enemyStats.HealthCurrent() <= 0)
+        {
+            stats.ChangeMoney((int)Mathf.Round(enemyStats.HealthMax() * Random.Range(0.25f, 0.5f)));
+            if (currentEnemy == 10) {currentEnemy = 0; enemyStats.NewBossStats(stats); }
+            else enemyStats.NewStats(stats);
+            stats.FullHeal();
+        }
+
+        if (stats.healthCurrent <= 0)
+        {
+            GameOver();
+        }
     }
+  
     void UpdateUIDisplay()
     {
         // Stats
@@ -75,6 +111,11 @@ public class GameManager : MonoBehaviour
         strengthUpgradeTexts[currentIndex].SetText("Strength $"+strengthCosts[currentIndex]+" for "+(int)Mathf.Round(strengthCosts[currentIndex]*0.05f*(currentIndex+0.5f)));
         defenceUpgradeTexts[currentIndex].SetText("Defence $"+defenceCosts[currentIndex]+" for "+(int)Mathf.Round(defenceCosts[currentIndex]*0.03f*(currentIndex+0.5f)));
         healthUpgradeTexts[currentIndex].SetText("Health $"+healthCosts[currentIndex]+" for "+(int)Mathf.Round(healthCosts[currentIndex]*0.015f*(currentIndex+0.5f)));
+
+        // Enemy
+        enemySlider.maxValue = enemyStats.HealthMax();
+        enemySlider.value = enemyStats.HealthCurrent();
+        enemySliderText.SetText(enemyStats.HealthCurrent()+"/"+enemyStats.HealthMax());
     }
 
     #region Upgrade Purchases
@@ -82,7 +123,7 @@ public class GameManager : MonoBehaviour
     public void PurchaseSpeed(int pageIndex)
     {
         int cost = speedCosts[pageIndex];
-        int change = (int)Mathf.Round(cost * 0.05f * (pageIndex + 0.5f) ); // multiplying in page index means higher
+        int change = (int)Mathf.Round(cost * 0.05f * (pageIndex + 0.5f)); // multiplying in page index means higher
         if (stats.Money() < cost) return;                                  // tiers actually provide more value (+0.5 so index 0 has value)
         stats.ChangeMoney(-cost);                                          // Does have the issue(?) of prices and value going up heaaaps
         stats.ChangeSpeed(change);
@@ -91,7 +132,7 @@ public class GameManager : MonoBehaviour
     public void PurchaseStrength(int pageIndex)
     {
         int cost = strengthCosts[pageIndex];
-        int change = (int)Mathf.Round(cost * 0.05f);
+        int change = (int)Mathf.Round(cost * 0.05f * (pageIndex + 0.5f));
         if (stats.Money() < cost) return;
         stats.ChangeMoney(-cost);
         stats.ChangeStrength(change);
@@ -100,7 +141,7 @@ public class GameManager : MonoBehaviour
     public void PurchaseDefence(int pageIndex)
     {
         int cost = defenceCosts[pageIndex];
-        int change = (int)Mathf.Round(cost * 0.05f);
+        int change = (int)Mathf.Round(cost * 0.05f * (pageIndex + 0.5f));
         if (stats.Money() < cost) return;
         stats.ChangeMoney(-cost);
         stats.ChangeDefence(change);
@@ -109,7 +150,7 @@ public class GameManager : MonoBehaviour
     public void PurchaseHealth(int pageIndex)
     {
         int cost = healthCosts[pageIndex];
-        int change = (int)Mathf.Round(cost * 0.05f);
+        int change = (int)Mathf.Round(cost * 0.05f * (pageIndex + 0.5f));
         if (stats.Money() < cost) return;
         stats.ChangeMoney(-cost);
         stats.ChangeHealth(change);
@@ -158,6 +199,41 @@ public class GameManager : MonoBehaviour
 
     #endregion
 
+    #region Attack Handling
+
+    void HurtEnemy(int damage, int atkSpeed)
+    {
+        float dur = 1f / atkSpeed;
+        tEnemy += Time.deltaTime;
+        while (tEnemy >= dur)
+        {
+            tEnemy -= dur;
+            enemyStats.ChangeHealthCurrent(-(int)Mathf.Round(damage * 0.5f));
+        }
+    }
+
+    public void HurtEnemyManual()
+    {
+        enemyStats.ChangeHealthCurrent(-stats.Strength());
+    }
+
+    void HurtPlayer(int damage, int atkSpeed)
+    {
+        float dur = 1f / atkSpeed;
+        tPlayer += Time.deltaTime;
+        while (tPlayer >= dur)
+        {
+            tPlayer -= dur;
+            stats.ChangeHealthCurrent((int)Mathf.Min(stats.Defence() - damage, 0));
+        }
+    }
+
+    #endregion
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("Gameover");
+    }
 }
 
 [System.Serializable]
@@ -189,10 +265,11 @@ public struct PlayerStats
     public void ChangeDefence(int change) { defence += change; }
     public int Defence() { return defence; }
 
+    public void FullHeal() {healthCurrent = healthMax;}
     public void SetHealthMax(int change) {healthMax = change;}
     public void SetHealthCurrent(int change) {healthCurrent = change;}
     public void ChangeHealth(int change) { healthMax += change; healthCurrent += change; }
-    public void ChangeHealthCurrent(int change) { healthCurrent += change; }
+    public void ChangeHealthCurrent(int change) { healthCurrent += change;}
     public int HealthMax () { return healthMax; }
     public int HealthCurrent() { return healthCurrent; }
 
@@ -200,4 +277,65 @@ public struct PlayerStats
     public int Money() { return money; }
 
     #endregion
+}
+
+[System.Serializable]
+public struct EnemyStats
+{
+    public int speed, strength, defence, healthMax, healthCurrent;
+
+    public EnemyStats(int speed, int strength, int defence, int healthMax)
+    {
+        this.speed = speed;
+        this.strength = strength;
+        this.defence = defence;
+        this.healthMax = healthMax;
+        this.healthCurrent = healthMax;
+    }
+
+    #region New Stats
+
+    public void NewStats(PlayerStats player)
+    {
+        speed = (int)Mathf.Round(player.Speed() * Random.Range(0.75f, 1.1f));
+        strength = (int)Mathf.Round(player.Strength() * Random.Range(0.7f, 1f));
+        defence = (int)Mathf.Round(player.Defence() * Random.Range(0.9f, 1.2f));
+        healthMax = (int)Mathf.Round(player.HealthMax() * Random.Range(1f, 1.75f) + strength * Random.Range(0.25f, 0.75f) + speed * Random.Range(0.25f, 0.75f));
+        healthCurrent = healthMax;
+    }
+
+    public void NewBossStats(PlayerStats player)
+    {
+        speed = (int)Mathf.Round(player.Speed() * Random.Range(1.15f, 2f));
+        strength = (int)Mathf.Round(player.Strength() * Random.Range(1.1f, 1.9f));
+        defence = (int)Mathf.Round(player.Defence() * Random.Range(1.4f, 2.4f));
+        healthMax = (int)Mathf.Round(player.HealthMax() * Random.Range(1.6f, 2.75f) + strength * Random.Range(1f, 1.5f) + speed * Random.Range(1.2f, 1.6f));
+        healthCurrent = healthMax;
+    }
+
+    #endregion
+
+    #region Get/Set Stats
+
+    public void SetSpeed(int change) {speed = change;}
+    public void ChangeSpeed(int change) { speed += change; }
+    public int Speed() { return speed; }
+
+    public void SetStrength(int change) {strength = change;}
+    public void ChangeStrength(int change) { strength += change; }
+    public int Strength() { return strength; }
+
+    public void SetDefence(int change) {defence = change;}
+    public void ChangeDefence(int change) { defence += change; }
+    public int Defence() { return defence; }
+
+    public void SetHealthMax(int change) {healthMax = change;}
+    public void SetHealthCurrent(int change) {healthCurrent = change;}
+    public void ChangeHealth(int change) { healthMax += change; healthCurrent += change; }
+    public void ChangeHealthCurrent(int change) { healthCurrent += change; }
+    public int HealthMax () { return healthMax; }
+    public int HealthCurrent() { return healthCurrent; }
+
+    #endregion
+
 }
