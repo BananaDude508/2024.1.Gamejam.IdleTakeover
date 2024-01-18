151d4d08dc0f73b365c754f5a57f4bcd2586a3fe
BananaDude508 <malsebhal@gmail.com>
2024-01-18 10:37:04 +1030

    Added various stats and shop pages updates
    Added the ability to manually add shop pages that automatically determine cost.
    Added the ability to switch between shop pages ingame.
    Decreased the price from 9x the previous page to 4x.
    Varied the value of each upgrade from 0.05x to 0.01x, 0.05x, 0.03x and 0.015x for different stats
    Updated upgrade methods to actually decrease your money
    Fixed GetNexUnwrappedIndex() to not throw an out of bounds error

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,41 +7,72 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    int shopPages = 1;
+    GameObject[] shopList;
     int[] speedCosts, strengthCosts, defenceCosts, healthCosts;
+    TextMeshProUGUI[] speedUpgradeTexts, strengthUpgradeTexts, defenceUpgradeTexts, healthUpgradeTexts;
+    int currentIndex = 0;
 
 
     PlayerStats stats = new PlayerStats(1, 10, 0, 100, 1000);
 
     public TextMeshProUGUI moneyText;
-    public TextMeshProUGUI speedText, strengthText, defenceText, healthText;
+    public TextMeshProUGUI speedText, strengthText, defenceText, healthText, healthSliderText;
     public Slider healthSlider;
 
     void Start()
     {
-        speedCosts = new int[shopPages];
-        strengthCosts = new int[shopPages];
-        defenceCosts = new int[shopPages];
-        healthCosts = new int[shopPages];
+        speedCosts = new int[shopList.Length];
+        strengthCosts = new int[shopList.Length];
+        defenceCosts = new int[shopList.Length];
+        healthCosts = new int[shopList.Length];
+
+        speedUpgradeTexts = new TextMeshProUGUI[shopList.Length];
+        strengthUpgradeTexts = new TextMeshProUGUI[shopList.Length];
+        defenceUpgradeTexts = new TextMeshProUGUI[shopList.Length];
+        healthUpgradeTexts = new TextMeshProUGUI[shopList.Length];
 
         speedCosts[0] = 100;
         strengthCosts[0] = 100;
         defenceCosts[0] = 100;
         healthCosts[0] = 100;
-        for (int i = 1; i < shopPages; i++)
+        for (int i = 1; i < shopList.Length; i++)
         {
-            speedCosts[i] = speedCosts[i-1] * 9 * i;
-            strengthCosts[i] = speedCosts[i-1] * 9 * 1;
-            defenceCosts[i] = speedCosts[i-1] * 9 * i;
-            healthCosts[i] = speedCosts[i-1] * 9 * i;
+            speedCosts[i] = speedCosts[i-1] * 4 * i;
+            strengthCosts[i] = speedCosts[i-1] * 4 * 1;
+            defenceCosts[i] = speedCosts[i-1] * 4* i;
+            healthCosts[i] = speedCosts[i-1] * 4 * i;
         }
-
-
+        for (int i = 0; i < shopList.Length; i++)
+        {
+            speedUpgradeTexts[i] = shopList[i].transform.GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            strengthUpgradeTexts[i] = shopList[i].transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            defenceUpgradeTexts[i] = shopList[i].transform.GetChild(2).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            healthUpgradeTexts[i] = shopList[i].transform.GetChild(3).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        }
     }
 
     void Update()
     {
         UpdateUIDisplay();
+    }
+        void UpdateUIDisplay()
+    {
+        // Stats
+        moneyText.SetText(stats.Money().ToString());
+        speedText.SetText(stats.Speed().ToString());
+        strengthText.SetText(stats.Strength().ToString());
+        defenceText.SetText(stats.Defence().ToString());
+        healthText.SetText(stats.HealthMax().ToString());
+        
+        // Slider
+        healthSliderText.SetText(stats.HealthCurrent()+"/"+stats.HealthMax());
+        healthSlider.value = stats.HealthMax() / stats.HealthCurrent();
+
+        // Upgrade Texts
+        speedUpgradeTexts[currentIndex].SetText("Speed $"+speedCosts[currentIndex]+" for "+(int)Mathf.Round(speedCosts[currentIndex]*0.01f*(currentIndex+0.5f)));
+        strengthUpgradeTexts[currentIndex].SetText("Strength $"+strengthCosts[currentIndex]+" for "+(int)Mathf.Round(strengthCosts[currentIndex]*0.05f*(currentIndex+0.5f)));
+        defenceUpgradeTexts[currentIndex].SetText("Defence $"+defenceCosts[currentIndex]+" for "+(int)Mathf.Round(defenceCosts[currentIndex]*0.03f*(currentIndex+0.5f)));
+        healthUpgradeTexts[currentIndex].SetText("Health $"+healthCosts[currentIndex]+" for "+(int)Mathf.Round(healthCosts[currentIndex]*0.015f*(currentIndex+0.5f)));
     }
 
     #region Upgrade Purchases
@@ -50,16 +81,17 @@ public class GameManager : MonoBehaviour
     {
         int cost = speedCosts[pageIndex];
         int change = (int)Mathf.Round(cost * 0.05f * (pageIndex + 0.5f) ); // multiplying in page index means higher
-        if (stats.Money() < cost) return;                                 // tiers actually provide more value (+0.5 so index 0 has value)
+        if (stats.Money() < cost) return;                                  // tiers actually provide more value (+0.5 so index 0 has value)
+        stats.ChangeMoney(-cost);                                          // Does have the issue(?) of prices and value going up heaaaps
         stats.ChangeSpeed(change);
         speedCosts[pageIndex] = (int)Mathf.Round(cost * 1.1f);
     }
-
     public void PurchaseStrength(int pageIndex)
     {
         int cost = strengthCosts[pageIndex];
         int change = (int)Mathf.Round(cost * 0.05f);
         if (stats.Money() < cost) return;
+        stats.ChangeMoney(-cost);
         stats.ChangeStrength(change);
         strengthCosts[pageIndex] = (int)Mathf.Round(cost * 1.1f);
     }
@@ -68,6 +100,7 @@ public class GameManager : MonoBehaviour
         int cost = defenceCosts[pageIndex];
         int change = (int)Mathf.Round(cost * 0.05f);
         if (stats.Money() < cost) return;
+        stats.ChangeMoney(-cost);
         stats.ChangeDefence(change);
         defenceCosts[pageIndex] = (int)Mathf.Round(cost * 1.1f);
     }
@@ -76,37 +109,50 @@ public class GameManager : MonoBehaviour
         int cost = healthCosts[pageIndex];
         int change = (int)Mathf.Round(cost * 0.05f);
         if (stats.Money() < cost) return;
+        stats.ChangeMoney(-cost);
         stats.ChangeHealth(change);
         healthCosts[pageIndex] = (int)Mathf.Round(cost * 1.1f);
     }
 
-    void UpdateUIDisplay()
+    #endregion
+
+    #region Shop Pages Handling
+
+    #region Get Unwrapped Index Helpers
+
+    int GetNextUnwrappedIndex()
     {
-        moneyText.SetText(stats.Money().ToString());
-        speedText.SetText(stats.Speed().ToString());
-        strengthText.SetText(stats.Strength().ToString());
-        defenceText.SetText(stats.Defence().ToString());
-        healthText.SetText(stats.HealthCurrent().ToString());
-        healthSlider.value = stats.HealthMax() / stats.HealthCurrent() * 0.01f;
+        return (int)Mathf.Min(currentIndex + 1, shopList.Length - 1);
+        // if (currentIndex + 1 >= shopList.Length) return shopList.Length;
+        // return currentIndex + 1;
+    }
+
+    int GetPreviousUnwrappedIndex()
+    {
+        return (int)Mathf.Max(currentIndex - 1, 0);
+        // if (currentIndex - 1 < 0) return 0;
+        // return currentIndex - 1;
     }
 
     #endregion
 
-    #region Get Unwrapped Index Helpers
+    #region Page Switchers
 
-    int GetNextUnwrappedIndex(int currentIndex)
+    public void NextShopPage()
     {
-        return Mathf.Min(currentIndex + 1, shopPages);
-        // if (currentIndex + 1 >= shopPages) return shopPahes;
-        // return currentIndex + 1;
+        shopList[currentIndex].SetActive(false);
+        currentIndex = GetNextUnwrappedIndex();
+        shopList[currentIndex].SetActive(true);
     }
 
-    int GetPreviousUnwrappedIndex(int currentIndex)
+    public void PreviousShopPage()
     {
-        return Mathf.Max(currentIndex - 1, 0);
-        // if (currentIndex - 1 < 0) return 0;
-        // return currentIndex - 1;
+        shopList[currentIndex].SetActive(false);
+        currentIndex = GetPreviousUnwrappedIndex();
+        shopList[currentIndex].SetActive(true);
     }
+
+    #endregion
 
     #endregion
 
