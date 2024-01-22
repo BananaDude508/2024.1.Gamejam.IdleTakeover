fea2cc22cc3dbbdb76ceed1f93bf0c72d87b264a
Cameron <cameron@ballfamily.tv>
2024-01-22 11:01:02 +1030

    Create StartButton.cs
    This code is for the play button to be able to start the game, change "Game" on line 7 and 9 for the same name the game scene is. 22/1/24 11 am.

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public void LoadScene(string Game)
+    {
+        SceneManager.LoadScene(Game)
+    }
+}
